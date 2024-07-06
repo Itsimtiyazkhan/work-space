2abe58d1872ca5813254503873e92a5c80606cc2
Imtiyaz-BriBooks <imtiyaz@bribooks.com>
2024-07-06 13:09:27 +0530

    change subtitle

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={`${styles.description}`}>Imtiyaz</div>
+      <div className={`${styles.description}`}>Imtiyaz khan</div>
 
       <div className={styles.center}>
         <Image
